bfe9edaa1098e25455152613c0a81775cc8c5253
maheshkvenkat <venkatmahesh808@gmail.com>
2024-03-19 18:28:53 +0530

    New Connection task

---
+++ src/testing/NewOrder.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class NewOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is for New Connection Scenario");
+	}
+
+}
